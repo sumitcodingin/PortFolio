8500a65786b0af244c80a656fdd0b80d793a426f
Sumit Sharma <sharmasumit0118@gmail.com>
2025-12-16 14:26:27 +0530

    layout completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar, Welcome } from '#components'
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Welcome />
+    </main>
+  ) 
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import Navbar from '#components/Navbar.jsx';
+import Welcome from '#components/Welcome.jsx';
+export { Navbar, Welcome };
